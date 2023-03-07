70ea9cb22a64d144d59bd36a1c0b9dc9e05c68a8
Jabbeer Jeerooburkhan <jabbeerjeerooburkhan@Jabbeers-iMac.local>
2023-03-07 12:27:01 +0000

    first upload

---
+++ food-delivery/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from '../components/Nav'
+import Searchbar from '../components/Searchbar'
+
+
+export default function Home() {
+  return (
+    <>
+      <Nav />
+      <Searchbar />
+    </>
+  )
+}
